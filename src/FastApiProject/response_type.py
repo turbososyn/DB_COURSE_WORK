57d8735dfa3988b85565b7ddecec70d69484900c
turboss <143201594+turbososyn@users.noreply.github.com>
2024-12-24 14:33:25 +0200

    removed

--- src/FastApiProject/response_type.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    lastname: str
-
-    class Config:
-        from_attributes = True
--- src/FastApiProject/user_create_class.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class UserCreate(BaseModel):
-    name: str
-    lastname: str
-    password: str
-
